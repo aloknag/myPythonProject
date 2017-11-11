5002b1f309f268fcbc6e4ab446b3fd0ec7ff6689
alok-nag <anag@akamai.com>
2017-11-11 14:35:05 +0530

    Clean up

--- FirstOne.py
+++ FirstOne.py
@@ -1,18 +1,18 @@
 x = 2
-numberofprimes = 0
+number_of_primes = 0
 primes = []
 
 
-def isPrime(x):
+def is_prime(x):
     for y in range(1, x):
         if x % y == 0 and y != 1:
             return False
     return True
 
 
-while numberofprimes < 1000:
-    if isPrime(x):
-        numberofprimes += 1
+while number_of_primes < 1000:
+    if is_prime(x):
+        number_of_primes += 1
         primes.append(x)
     x += 1
 
@@ -24,7 +24,7 @@ while row + col < 1000:
         col += 1
         if col == 10:
             print "\n"
-            col = 0;
+            col = 0
             row = row + 10
 
 
